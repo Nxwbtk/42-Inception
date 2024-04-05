3dde61aecb5abbc6fbe62adaecb3ea04f16db8fe
bsirikam <buntakan0703@gmail.com>
2024-04-06 01:18:36 +0700

    Update Docker configurations and WordPress setup

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -4,10 +4,12 @@ wget https://th.wordpress.org/latest-th.tar.gz
 tar -xpvf latest-th.tar.gz
 mv wordpress/* .
 rm -rf latest-th.tar.gz wordpress
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-sed -i -r "s/database/$DB_NAME/1"   wp-config.php
-sed -i -r "s/database_user/$DB_USER/1"  wp-config.php
-sed -i -r "s/passwod/$DB_PASS/1"    wp-config.php
+chown -R www-data:www-data *
+chmod -R 777 *
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+sed -i -r "s/database_name_here/$DB_NAME/1"   wp-config.php
+sed -i -r "s/username_here/$DB_USER/1"  wp-config.php
+sed -i -r "s/password_here/$DB_PASS/1"    wp-config.php
 sed -i -r "s/localhost/mariadb/1"    wp-config.php
 wp core install --url=$DOMAIN_NAME/ --title=$WP_NAME --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
