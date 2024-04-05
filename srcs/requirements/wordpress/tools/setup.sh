dd5f8e5bc591e9c543fc0aa0472a68daea726897
bsirikam <65420147@kmitl.ac.th>
2024-04-05 18:22:00 +0700

    update

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdir /var/www/
-mkdir /var/www/html
 cd /var/www/html
 rm -rf *
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -9,9 +7,11 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-sed -i -r "s/database/$db_name/1"   wp-config.php
-sed -i -r "s/database_user/$db_user/1"  wp-config.php
-sed -i -r "s/passwod/$db_pwd/1"    wp-config.php
+sed -i -r "s/database/$DB_NAME/1"   wp-config.php
+sed -i -r "s/database_user/$DB_USER/1"  wp-config.php
+sed -i -r "s/passwod/$DB_PASS/1"    wp-config.php
 sed -i -r "s/localhost/mariadb/1"    wp-config.php
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+wp core install --url=$DOMAIN_NAME/ --title=$WP_NAME --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
+
+exec "$@"
